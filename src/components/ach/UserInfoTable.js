f908038339c88b713f4e282db4b6e08361ae0783
Jacobo Vasquez <javasquez@bancatlan.hn>
2020-12-04 23:04:42 -0600

    style and layout changes

--- src/components/ach/UserInfoTable.js
+++ src/components/ach/UserInfoTable.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { Descriptions } from 'antd';
 
-// const UserInfoTable = ({info}) => {
 const UserInfoTable = info => {
     const { customerCoreCitizen, customerCoreEmail, customerCoreMarital, customerCoreName, customerCoreNationality, customerCorePhone, customerOCBUser } = info;
     return (
-        <Descriptions title="Información del Usuario" layout="vertical" bordered>
+        <Descriptions
+            title="Información del Usuario"
+            layout="vertical"
+            size="small"
+            column={{ xxl: 4, xl: 4, lg: 4, md: 3, sm: 1, xs: 1 }}
+            bordered
+        >
             <Descriptions.Item label="Identidad">{customerCoreCitizen}</Descriptions.Item>
             <Descriptions.Item label="Correo eléctronico">{customerCoreEmail}</Descriptions.Item>
             <Descriptions.Item label="Estado civil">{customerCoreMarital}</Descriptions.Item>
