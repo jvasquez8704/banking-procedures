e4ad446c6e196fd760e7f84407990eb9e39fb81b
Jacobo Vasquez <javasquez@bancatlan.hn>
2021-02-19 16:58:29 -0600

    update text for new user modal, centering text on generic modal

--- src/components/ui/Content.js
+++ src/components/ui/Content.js
@@ -35,11 +35,11 @@ const StepScreen = ({ steps, desc }) => {
                     visible={err && true}
                     onCancel={hideModal}
                     footer={null}
-                    className="stc-error-modal"
+                    className="stc-error-modal stc-modal"
                 >
                     <img alt="Error img" className="stc-err-icon" src={iconErr} />
                     <p className="stc-err-title">¡Error!</p>
-                    <p>{err}</p>
+                    <p style={{textAlign:'center'}}>{err}</p>
                 </Modal>
             </div>
             <Card title={desc} className="stc-card-widget">
